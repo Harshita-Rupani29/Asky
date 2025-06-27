79176dee3ed3e8f93754ccf72b13ac387d00995c
Harshita <harshitarupani4@gmail.com>
2025-06-28 00:46:27 +0530

    Fix case-sensitive import for Footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Header from '../components/Header';
 import LandingPage from '../components/LandingHero';
 import Why from '../components/LandingWhy';
-import Footer from '../components/Footer';
+import Footer from '../components/Footer'
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#f7f9fb]">
